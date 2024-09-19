148a0512fe5b0117f78ddb8575db07fac0d59e9e
Alex Zaitsev <alex.zaitsev.pro@gmail.com>
2024-09-18 22:12:51 -0600

    v2.0.0: conference cards

--- main-model.go
+++ main-model.go
@@ -5,11 +5,12 @@ import "google.golang.org/genproto/googleapis/type/latlng"
 type Conference struct {
 	Name       string         `firestore:"name"`
 	Location   string         `firestore:"location"`
+	PictureUrl string         `firestore:"picture_url"`
+	Flag       string         `firestore:"flag"`
 	Continent  string         `firestore:"continent"`
 	Coords     *latlng.LatLng `firestore:"coords"`
 	StartDate  string         `firestore:"start_date"`
 	EndDate    string         `firestore:"end_date"`
 	Tz         string         `firestore:"tz"`
 	Url        string         `firestore:"url"`
-	PictureUrl string         `firestore:"url_picture"`
 }
