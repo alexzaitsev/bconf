bc50ce9edb70c710b208559cdacecec21b12eb30
Alex Zaitsev <alex.zaitsev.pro@gmail.com>
2024-09-20 12:08:51 -0600

    v2.0.0: conference cards

--- static/js/conferences.js
+++ static/js/conferences.js
@@ -1,5 +1,30 @@
+let preloadedCardHTML = '';
+let isTemplateLoading = false;
+
+function loadTemplate(callback) {
+    if (preloadedCardHTML === "" && !isTemplateLoading) {
+        isTemplateLoading = true;
+        $.get('/static/html/conference_card.html', function (data) {
+            preloadedCardHTML = data;
+            isTemplateLoading = false; // Reset the flag once the template is loaded
+            callback();
+        }).fail(function () {
+            isTemplateLoading = false; // Reset the flag in case of an error
+            console.error("Failed to load the template");
+        });
+    } else if (preloadedCardHTML !== "") {
+        // If the template is already loaded, call the callback immediately
+        callback();
+    }
+}
+
 function updateCards(conferences) {
-    const container = $('#conference-cards')
-    container.empty()
-    conferences.forEach(conf => { addCard(container, conf) })
+    const container = $('#conference-cards');
+    container.empty();
+
+    loadTemplate(function () {
+        conferences.forEach(conf => {
+            addCard(container, preloadedCardHTML, conf);
+        });
+    });
 }
